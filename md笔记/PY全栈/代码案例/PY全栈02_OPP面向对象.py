449318191d0906b5eb22dab68092ff17ab601028
liuning <liu2946181970@126.com>
2018-11-19 17:04:55 +0800

    Py学习:OOP类

--- md笔记/PY全栈/代码案例/PY全栈02_OPP面向对象.py
+++ md笔记/PY全栈/代码案例/PY全栈02_OPP面向对象.py
@@ -1,13 +1,18 @@
+# %%
 # 空'类'
+
+
 class studert():
     # 如果类为空,则必须有`pass``
     pass
 
 
 stu_xiaoming = studert()
-
-
+del stu_xiaoming
+# %%
 # 标准'类'
+
+
 class student():
 
     name = None
@@ -15,12 +20,21 @@ class student():
     gender = 'man'
     course = 'Python'
 
-    def doHomeWork():
+    # 这个函数必须有(self)的变量传入
+    def doHomeWork(self):
         print('我在做作业')
         return None
 
 
+# 设置学生:'域名'
 yu_min = student()
 yu_min.name = '域名'
+
+print('"域名"的信息:')
 print(yu_min.name)
 yu_min.doHomeWork()
+# 设置学生:'芦苇'
+
+# %%
+print('student的所有实例为:')
+student.__dict__
